cf8e4eb5bb317c1b703cc109e410829b3aa62e7d
pash bespaloff <123310036+pashbespaloff@users.noreply.github.com>
2023-04-04 20:59:17 +0200

    04.04.2023

--- js/calc.js
+++ js/calc.js
@@ -16,10 +16,12 @@ btnResult.addEventListener("click", getResult);
 // going through arrays to add correct digit or operator
 btnNumbers.forEach(item => item.addEventListener("click", writeToInput));
 btnOperators.forEach(item => item.addEventListener("click", writeToInput));
+document.addEventListener("keydown", writeKeysToInput);
 
+/*
 function writeToInput(event) {
-  let symbol = event.target.textContent;
-      input = calcField.value;
+  let symbol = event.target.textContent,
+      input = calcField.value,
       lastSymbol = input[input.length - 1];
   if (event.target.classList.contains("btn-operator")) {
     if (lastSymbol !== " " && input.length) {
@@ -37,6 +39,31 @@ function writeToInput(event) {
   };
   calcField.value = input;
 };
+*/
+
+function writeToInput(event) {
+  let input = calcField.value,
+      symbol = event.target.textContent,
+      lastSymbol = input[input.length - 1];
+  const isOperator = event.target.classList.contains("btn-operator"),
+        isNewValue = input.length === 0 || lastSymbol === " ";
+
+  if (isOperator) {
+    if (isNewValue) {
+      console.log("enter correct value");
+    } else {
+    input += ` ${symbol} `;
+    };
+  } else {
+    if (isNewValue && symbol === "0") {
+      console.log("enter correct value");
+    } else {
+    input += symbol;
+    };
+  };
+
+  calcField.value = input;
+};
 
 function deleteSymbol() {
   let input = calcField.value,
@@ -49,6 +76,7 @@ function deleteSymbol() {
   calcField.value = input;
 };
 
+/*
 function getResult () {
   let input = calcField.value,
       arrayOfInput = input.split(" "),
@@ -62,4 +90,71 @@ function getResult () {
       operatorsArray.push(arrayOfInput[i]);
     };
   };
+};
+*/
+
+function getResult () {
+  let input = calcField.value,
+      expression = input.split(" ").join(""),
+      result = eval(expression);
+
+  input = result;
+  calcField.value = input;
+
+  restartCalc();
+};
+
+function restartCalc () {
+  btnNumbers.forEach(item => item.removeEventListener("click", writeToInput));
+  btnOperators.forEach(item => item.removeEventListener("click", writeToInput));
+  document.removeEventListener("keydown", writeKeysToInput);
+
+  btnNumbers.forEach(item => item.addEventListener("click", cleanInput));
+  btnOperators.forEach(item => item.addEventListener("click", cleanInput));
+  document.addEventListener('keydown', cleanInput);
+
+  function cleanInput() {
+    input = "";
+    calcField.value = input;
+    btnNumbers.forEach(item => item.removeEventListener("click", cleanInput));
+    btnOperators.forEach(item => item.removeEventListener("click", cleanInput));
+    document.removeEventListener('keydown', cleanInput);
+  };
+
+  btnNumbers.forEach(item => item.addEventListener("click", writeToInput));
+  btnOperators.forEach(item => item.addEventListener("click", writeToInput));
+  document.addEventListener("keydown", writeKeysToInput);
+};
+
+function writeKeysToInput(event) {
+  let input = calcField.value, 
+      key = event.key;
+      lastSymbol = input[input.length - 1];
+  
+  const isOperator = key === "+" || key === "-" || key === "*" || key === "/",
+        isNumber = key >= 0 && key <= 9,
+        isNewValue = input.length === 0 || lastSymbol === " ";
+
+  if (isOperator) {
+    if (isNewValue) {
+      console.log("enter correct value");
+    } else {
+    input += ` ${key} `;
+    };
+    
+  } else if (isNumber) {
+    if (isNewValue && key === "0") {
+      console.log("enter correct value");
+    } else {
+    input += key;
+    };
+  };
+
+  calcField.value = input;
+
+  if (key === "Backspace")
+    deleteSymbol();
+
+  if (key === "=")
+    getResult();
 };
